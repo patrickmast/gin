b2b2f7a9b0b067e6784d70a363ad2d4c4e1d1c89
Patrick Mast <patrick@winfakt.com>
2023-06-24 19:17:20 +0200

    Update main.go

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 		})
 	})
 
-	r.POST("/Test", func(c *gin.Context) {
+	r.GET("/test", func(c *gin.Context) {
 		currentTime := time.Now().Format("2006-01-02 15:04:05")
 		c.JSON(200, gin.H{
 			"current_time": currentTime,

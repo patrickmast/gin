21e4268c9c074a87c27679e146aa88c39565ab9b
Patrick Mast <patrick@winfakt.com>
2023-06-24 19:16:11 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
-var Router * gin.Engine
+
+var Router *gin.Engine
+
 func main() {
 	r := gin.Default()
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello world!",
 		})
 	})
+
+	r.POST("/Test", func(c *gin.Context) {
+		currentTime := time.Now().Format("2006-01-02 15:04:05")
+		c.JSON(200, gin.H{
+			"current_time": currentTime,
+		})
+	})
+
 	r.Run()
 }
